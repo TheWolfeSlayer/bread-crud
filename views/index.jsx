581ab556ecc2099db6d541cabbdfd7864aa92a75
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-05-02 21:11:43 -0400

    can add bread

--- views/index.jsx
+++ views/index.jsx
@@ -12,6 +12,11 @@ function Index ({ breads }) {
     return (
       <Default>
         <h2>Index Page</h2>
+        <a href='/breads/new'>
+            <button>
+                Add a new bread
+            </button>
+        </a>
         <ul>
             {display}    
         </ul>
